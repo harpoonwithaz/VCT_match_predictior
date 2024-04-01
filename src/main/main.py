7a0c502712d0822d80b0966ec777aa58408b983d
harpoonwithaz <otad19@gmail.com>
2024-03-31 22:07:16 -0700

    Update main.py
    I added stuff

--- src/main/main.py
+++ src/main/main.py
@@ -1,5 +1,21 @@
 # VCT match outcome prediction algorithm
 
 import requests
+import json
 import pandas as pd
 
+# Basic team data of all VCT teams
+all_teams = requests.get("https://vlr.orlandomm.net/api/v1/teams?region=all")
+
+def get_json_data(response):
+    # Check if the request was successful
+    if response.status_code == 200:
+        json_data = response.json()
+    else:
+        print("Failed to retrieve data from the API")
+        json_data = None
+    # Returns json data
+    return json_data
+
+test = get_json_data(all_teams)
+print(type(test))
