77912dba92727b3d715442f04ec2a866f5428e64
harpoonwithaz <otad19@gmail.com>
2024-04-24 23:22:51 -0700

    added fetch_all_events function, and updated some functions

--- src/main/api_modules/obtain_api_data.py
+++ src/main/api_modules/obtain_api_data.py
@@ -85,13 +85,14 @@ def fetch_match_results():
     return df
 
 # Function to get all VCT teams basic info from api, and return them as dataframe
-def fetch_all_teams(region: str):
+def fetch_all_teams(region: str = "all"):
     """
     Fetches all VCT teams basic info from API and returns them as a dataframe.
 
     Returns:
     A dataframe containing all VCT teams basic info.
     """
+
     start_time = time.time()  # Record the start time
     
     # Defines dataframe column names
@@ -101,7 +102,7 @@ def fetch_all_teams(region: str):
     df = pd.DataFrame(columns = df_columns)
 
     # Makes a request to the API
-    all_teams = requests.get(f'https://vlr.orlandomm.net/api/v1/teams?limit=all')
+    all_teams = requests.get(f'https://vlr.orlandomm.net/api/v1/teams?limit=all&region={region}')
     
     # Gets json data
     json_data = check_json.get_json_data(all_teams)
@@ -153,12 +154,52 @@ def fetch_team(team_id: int):
     return df
 
 # Function to get all players data from api, and returns them as dataframe
-def fetch_all_players():
+def fetch_all_players(result_limit = None,
+                    event_series_id : int = None, 
+                    specific_event_id : int = None, 
+                    #region : str = None, 
+                    country : str = None, 
+                    min_rounds : int = 0, 
+                    min_rating : int = 0,
+                    agent : str = None,
+                    map_id : int = None,
+                    time_span : str = None):  # Parameters for the URL, they are not required to be given args when calling the function
     """
-    Fetches all players data from api, and returns them as dataframe.
+    Fetches all players teams basic info from API and returns them as a dataframe.
+
+    Parameters (Args not required):
+    result_limit (int/str): Limit of results per page. You can also use all to get all players. If no args given, sets to 10 results.
+        This parameter represents the limit of results per page (Ex. all, 10, 15, 34, etc.) which is passed to the URL to make a call to the api containing the data with that parameter.
+
+    event_series_id (int): Specific ID of event series. If no args given, sets to all events.
+        This parameter represents the event series (Ex. Valorant Champions Tour 2024, Valorant Game Changers 2024, etc.) unique ID which is passed to the URL to make a call to the api containing the data with that parameter.
+
+    specific_event_id (int): Specific ID of specific event in the event series. If no args given, sets to all event series.
+        This parameter represents the specific event in the event series (Ex. Champions Tour 2024: Americas Kickoff, Champions Tour 2024: Masters Madrid, etc.) unique ID which is passed to the URL to make a call to the api containing the data with that parameter.
+
+    region (str): 2/3 letter abbreviation for players region. If no args given, sets to all regions. CURRENTLY NOT WORKING
+        This parameter represents the 2/3 letter abbreviation for players region (Ex. na, eu, oce, etc.) which is passed to the URL to make a call to the api containing the data with that parameter.
+
+    country (str): 2/3 letter abbreviation for players country. If no args given, sets to all countries.
+        This parameter represents the 2/3 letter abbreviation for players country (Ex. ca, us, etc.) which is passed to the URL to make a call to the api containing the data with that parameter.
+
+    min_rounds (int): Minimum rounds player played. By default is set to 0.
+        This parameter represents the minimum rounds a player has played which is passed to the URL to make a call to the api containing the data with that parameter.
+
+    min_rating (int): Minimum rating of player. By default is set to 0.
+        This parameter represents the minimum rating a player has which is passed to the URL to make a call to the api containing the data with that parameter.
+
+    agent (str): Specific agent. If no args given, sets to all agents.
+        This parameter represents one agent (Ex. jett, astra, etc.) a player has played which is passed to the URL to make a call to the api containing the data with that parameter.
+
+    map_id (int): Specific id of certain map. If no args given, sets to all maps.
+        This parameter represents the stats of a certain player on a certain map (Ex. split, bind, etc.) unique ID which is passed to the URL to make a call to the api containing the data with that parameter.
+
+    time_span (str): Time period. If no args given, sets to 60d.
+        This parameter represents the time period of players stats (Ex. all, 30d, 60d, etc.) which is passed to the URL to make a call to the api containing the data with that parameter.
 
     Returns:
-    A dataframe containing all players data.
+    A dataframe containing all players data with specific parameter.
     """
     start_time = time.time()  # Record the start time
     
@@ -168,8 +209,34 @@ def fetch_all_players():
     # Creates new data frame for all teams
     df = pd.DataFrame(columns = df_columns)
 
-    # Makes a request to the API
-    all_players = requests.get('https://vlr.orlandomm.net/api/v1/players?limit=all')
+    # Constructing the URL parameters dynamically
+    url_args = ""
+    params = [
+        ('limit', result_limit),
+        ('event_series_id', event_series_id),
+        ('specific_event_id', specific_event_id),
+        #('region', region),
+        ('country', country),
+        ('min_rounds', min_rounds),
+        ('min_rating', min_rating),
+        ('agent', agent),
+        ('map_id', map_id),
+        ('time_span', time_span)
+    ]
+    
+    # Add parameters to url_args if they are not None
+    for param_name, param_value in params:
+        if param_value is not None:
+            url_args += f"{param_name}={param_value}&"
+
+    # Remove the last '&' if url_args is not empty
+    if url_args:
+        url_args = '?' + url_args[:-1]
+
+    print(url_args)
+
+    # Makes a request to the API with the api url args
+    all_players = requests.get(f'https://vlr.orlandomm.net/api/v1/players{url_args}')
 
     # Gets json data
     json_data = check_json.get_json_data(all_players)
@@ -185,13 +252,14 @@ def fetch_all_players():
     # Returns json data
     return df
 
+# Function to get detailed player info on specified player from api and return it as a dataframe
 def fetch_player(player_id: int):
     """
     Fetches detailed info of specific player from API and returns them as a dataframe. Requires players unique ID.
 
     Parameters:
     player_id (int): Specific players unique ID.
-        This parameter represents the players unique ID to pass to the URL to make a call to the api containing the players specific data.
+        This parameter represents the players unique ID which is passed to the URL to make a call to the api containing the players specific data.
 
     Returns:
     A dataframe containing detailed info of specific player.
@@ -199,23 +267,76 @@ def fetch_player(player_id: int):
     start_time = time.time()  # Record the start time
     
     # Defines dataframe column names
-    df_columns = ['info', 'string', 'team', 'pastTeams', 'socials']
+    df_columns = ['info', 'team', 'results', 'pastTeams', 'socials']
 
     # Creates new data frame for all teams
     df = pd.DataFrame(columns = df_columns)
 
     # Makes a request to the API
-    player_data = requests.get(f'https://vlr.orlandomm.net/api/v1/teams/{player_id}')
+    player_data = requests.get(f'https://vlr.orlandomm.net/api/v1/players/{player_id}')
 
     # Gets json data
     json_data = check_json.get_json_data(player_data)
 
     # Appends json data to df and returns it
-    df = df._append(json_data['data'])
+    df = df._append(json_data['data'], ignore_index = True)
     
     # Record the end time, calculates execution time and prints it
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Execution time: {execution_time} seconds")
 
+    return df
+
+# Function to get details on events from api and return is as a dataframe
+def fetch_all_events(event_status : str = None, event_region : str = None):
+    """
+    Fetches details about valorant events from API and returns them as a dataframe.
+
+    Parameters:
+    event_status (int): Filters events by their status. 
+        This parameter represents the events status (ongoing, upcoming, completed, all) which is passed to the URL to make a call to the api containing the players specific data.
+
+    Returns:
+    A dataframe containing details on valorant events.
+    """
+    start_time = time.time()  # Record the start time
+    
+    # Defines dataframe column names
+    df_columns = ['id', 'name', 'status', 'prizepool', 'dates', 'country', 'img']
+
+    # Creates new data frame for all teams
+    df = pd.DataFrame(columns = df_columns)
+
+    # Constructing the URL parameters dynamically
+    url_args = ""
+    params = [
+        ('status', event_status),
+        ('region', event_region),
+    ]
+    
+    # Add parameters to url_args if they are not None
+    for param_name, param_value in params:
+        if param_value is not None:
+            url_args += f"{param_name}={param_value}&"
+
+    # Remove the last '&' if url_args is not empty
+    if url_args:
+        url_args = '?' + url_args[:-1]
+
+    # Makes a request to the API with the api url args
+    events = requests.get(f'https://vlr.orlandomm.net/api/v1/players?{url_args}')
+
+    # Gets json data
+    json_data = check_json.get_json_data(events)
+
+    # Appends json data to df
+    df = df._append(json_data['data'])
+
+    # Record the end time, calculates execution time and prints it
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time} seconds")
+
+    # Returns json data
     return df
--- src/main/main.py
+++ src/main/main.py
@@ -8,14 +8,18 @@ from api_modules import obtain_api_data
 #match_results_df = obtain_api_data.fetch_match_results()
 #match_results_df.to_csv(file_paths.api_match_results, index=False)
 
-#all_teams = obtain_api_data.fetch_all_teams()
+#all_teams = obtain_api_data.fetch_all_teams("na")
+#print(all_teams)
+
 
 # 2593 | Test team ID (Fnatic)
 #team_data = obtain_api_data.fetch_team(2593)
 #print(team_data)
 
-#all_players = obtain_api_data.fetch_all_players()
+
+#all_players = obtain_api_data.fetch_all_players(country = 'ca', agent = 'omen', result_limit = "all")
 #print(all_players)
 
-#
-#player_data = obtain_api_data.fetch_player()
+# Test ID: 9 (TenZ)
+#player_data = obtain_api_data.fetch_player(9)
+#print(player_data)
